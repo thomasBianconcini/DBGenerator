f27c9f3e0680816f8bec60b79f30ec534ab2c640
TommyMuz11 <106307317+TommyMuz11@users.noreply.github.com>
2023-12-09 10:56:54 +0100

    Add files via upload

--- llama_muz.py
+++ llama_muz.py
@@ -4,43 +4,62 @@ from langchain.chains import LLMChain
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 
-nt = input("quante tabelle vuoi generare?\n")
-natt = input("quanti attributi per ogni tabella?\n")
+nt = str(input("quante tabelle vuoi generare?\n"))
+natt = str(input("quanti attributi per ogni tabella?\n"))
 
-template = """Question: {question}
 
-Answer: Please list the names in a comma-separated format: name1, name2, name3. Output must be only values with nothing more."""
 
-prompt = PromptTemplate(template=template, input_variables=["question"])
+#prompt_t = PromptTemplate(template=template, input_variables=["question","nt","natt"])
 
 # Callbacks support token-wise streaming
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
 # Make sure the model path is correct for your system!
 llm = LlamaCpp(
-    model_path="/home/muz/llama.cpp/models/openorca-platypus2-13b.gguf.q4_0.bin",
+    model_path="/home/muz/llama.cpp/models/dolphin2.1-openorca-7b.Q4_K_M.gguf",
     temperature=0.75,
     max_tokens=2000,
     top_p=1,
     callback_manager=callback_manager,
     verbose=True,  # Verbose is required to pass to the callback manager
+    grammar_path = "list.gbnf"
 )
 
-#prompt = "   Generate me one last list of 4 rows of data to populate each table. The output must be a unique list of strings made only of names."
-#llm(prompt)
 
-#prompt_t = "I am creating a fake db about Users. Generate a list of "+ str(nt) +" random names of SQL tables. "
+#prompt = """
+ #           Given an input question, generate a list of strings of names related to that question. Write the List between <list></list>.
+#            
+#            Below are three example Questions and the corresponding outputs. 
+#            <example>
+#            Question: Give me a list of 3 names related to Users to use as table names in a db.
+#            output: <list>name1,name2,name3</list>
+#            </example>
+#            <example>
+#            Question: Give me a list of 4 names related to Users to use as table names in a db.
+#            output: <list>name1,name2,name3, name4</list>
+#           </example>
+#           <example>
+#            Question:Generate a list of 5 of random names related to Users to use in a db
+#            output: <list>name1,name2,name3, name4, name5</list>
+#            </example>
+#                        
+#            Please provide the SQL query for this question: 
+#            Question:Create a list of {nt} random  names to use as table names in a db.
+#            List:
+#"""
 
-#prompt_at = "Generate one more list of " + str(natt) + " attribute names for each table name you generated in the previouse prompt. "
+prompt= "Generate a list of table names related to a table users. the first table must be users and the others must be related to users. The length of the list must be " + nt + "."
+print(prompt)
+output = llm(prompt)
 
-#prompt_d = "Generate a list of strings of data to insert into the tables you just created. I need 2 rows for each table created. "
+# Processing the output to extract only table names
+table_names = output.replace("[]","").replace("]","").replace("\"","").strip().split(",")
 
-prompt_t = f"Generate {nt} names of SQL tables about Users in a comma-separated format."
-
-#prompt_at = f"Please provide a list of {natt} attribute names for each table, in a comma-separated format."
-
-#prompt_d = "Generate a comma-separated list of data strings to insert into the tables. I need 2 rows for each table."
-
-
-res = llm(prompt_t)
-
+print("Generated Table Names:")
+for name in table_names:
+    print(name)
+print("Starting attribute generation...")
+for name in table_names:
+    prompt_att = "Generate a list of attribute names related to the table "+ name+". . The length of the list must be "+natt + "."
+    output_att=llm(prompt_att)
+    print(name)
