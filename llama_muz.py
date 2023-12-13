fea40b4b29e1b70765abf4a49d76e0e769473a89
TommyMuz11 <106307317+TommyMuz11@users.noreply.github.com>
2023-12-13 13:56:51 +0100

    integrated data generation with Dif's magic Bot. Integrated Mockaroo's API Generate request.

--- llama_muz.py
+++ llama_muz.py
@@ -4,12 +4,18 @@ from langchain.chains import LLMChain
 from langchain.llms import LlamaCpp
 from langchain.prompts import PromptTemplate
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
+import requests
+import json
+
 nt = str(input("quante tabelle vuoi generare?\n"))
 natt = str(input("quanti attributi per ogni tabella?\n"))
 
-
-
-#prompt_t = PromptTemplate(template=template, input_variables=["question","nt","natt"])
 
 # Callbacks support token-wise streaming
 callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
@@ -25,28 +31,6 @@ llm = LlamaCpp(
     grammar_path = "list.gbnf"
 )
 
-
-#prompt = """
- #           Given an input question, generate a list of strings of names related to that question. Write the List between <list></list>.
-#            
-#            Below are three example Questions and the corresponding outputs. 
-#            <example>
-#            Question: Give me a list of 3 names related to Users to use as table names in a db.
-#            output: <list>name1,name2,name3</list>
-#            </example>
-#            <example>
-#            Question: Give me a list of 4 names related to Users to use as table names in a db.
-#            output: <list>name1,name2,name3, name4</list>
-#           </example>
-#           <example>
-#            Question:Generate a list of 5 of random names related to Users to use in a db
-#            output: <list>name1,name2,name3, name4, name5</list>
-#            </example>
-#                        
-#            Please provide the SQL query for this question: 
-#            Question:Create a list of {nt} random  names to use as table names in a db.
-#            List:
-#"""
 
 prompt= "Generate a list of table names related to a table users. the first table must be users and the others must be related to users. The length of the list must be " + nt + "."
 print(prompt)
@@ -74,39 +58,73 @@ for t in clean_att:
     for name in t:
         print(name)
 
-values =[]
-st_rows=[]
-atts= ""
 
-print("Generating values to populate the db...\n")
-for index,name in enumerate(table_names):
+#------------------------------------------------------# SCRIPT SELENIUM#------------------------------------------------------#
 
-    #prompt = "Generate a list of 8 random user IDs"
+#MODIFICHE PER RENDERE SELENIUM UNDETECTABLE
+firefox_capabilities = DesiredCapabilities.FIREFOX
+firefox_capabilities['marionette'] = False
+profile=webdriver.FirefoxProfile()
+profile.set_preference("dom.webdriver.enabled", False)
+profile.set_preference("useAutomationExtension", False)
+profile.update_preferences()
+
+#INIZIO SCRIPT SELENIUM
+options = Options()
+options.add_argument("--headless")
+#driver = webdriver.Firefox(firefox_profile=profile, options=options)
+driver = webdriver.Firefox(firefox_profile=profile)
+MOCKAROO_LINK = "https://www.mockaroo.com/"
+driver.get(MOCKAROO_LINK)
+
+driver.find_element(By.XPATH,f"//span[contains(., 'Generate fields using AI...')]").click()
+#el=driver.find_element(By.XPATH,f"//span[contains(., 'Generate fields using AI...')]").click().perform()
+#ActionChains(driver).move_to_element(element).click().perform()
+#driver.execute_script("arguments[0].click();", el)
+
+temp_att_list = sum(clean_att, [])
+
+attribute_list = []
+
+for idx, el in enumerate(temp_att_list):
+    attribute_list.append(f"{table_names[idx//int(natt)]}_{el}")                ##ho dovuto aggiungere "tablename"_ prima del nome dell'attributo perchè senno con attributi che si chiamavano uguale esplodeva
+    print(el)                                                                                  
 
 
+input_bar=driver.find_element(By.XPATH,f"//input[contains(@placeholder, 'Examples')]")
+input_bar.send_keys(str(attribute_list))
+input_bar2=driver.find_element(By.NAME, "count")
+input_bar2.clear()
+input_bar2.send_keys(str(len(attribute_list)))
+driver.find_element(By.XPATH,f"//span[contains(., 'Replace Existing Fields')]").click()
+sleep(15)
+divs=driver.find_elements(By.XPATH,f"//div/div[contains(@class, 'schema-column')]/div/button/span/div")
+types_list=[]
+#print("\n\n\n",attribute_list,"\n\n\n")
 
-    prompt = "###CONTEXT: I am creating a database. An ID is an integer number. a Date is a date of the calendar. I have the table " + name + "( \n ID INT PRIMARY KEY AUTOINCREMENT "
-    for j,att in enumerate(clean_att[index]):
-        atts = atts + " " + att
-        prompt = prompt + ",\n" + att + " VARCHAR(50)"
-    for i in range(0,2):
-        prompt = prompt + " ###QUESTION: Generate a list of values to insert in the table. The length of the list must be " +natt +"."
-        print("PROMPT COMPOSTO DINAMICAMENTE: " + prompt)
-        prompt_val = "Generate a list of values of data to use to populate a row of the table " + name + ". The table has these attributes: "+ atts + " . The length of the list must be " +natt +"."
-        output_val = llm(prompt)
-        st_rows.append(output_val.replace("[","").replace("]","").replace("\"","").strip().split(","))
-    values.append(st_rows)
-    st_rows=[]
+for div in divs:
+    types_list.append(div.text)
 
-    
+print("\n\n",types_list, "\n\n")
 
-            
-    #values.append()
+#driver.quit()#CHIUDE BROWSER--- 
 
-         
-   # values.append(st_rows)
-   # st_rows=[]
+my_key="c1a2daf0"
+url = f"""https://api.mockaroo.com/api/generate.json?key={my_key}&fields=["""
 
-for tr in values:
-    for v in tr:
-        print(v)
+i=0
+print("\n\nlunghezze liste: \n",len(attribute_list)," (att) - ",  len(types_list), "\n\n")
+for idx,el in enumerate(attribute_list):
+    url=url+ "{"+f'"name": "{el}","type": "{types_list[i]}"'+"}"                  
+    if i!=len(attribute_list)-1:
+        url=url+","
+    i=i+1
+url=url+"]&count="+str(len(attribute_list))
+print("\n\n")
+
+payload = {}
+headers = {}
+response = requests.request("POST", url, headers=headers, data=payload)
+dati = json.loads(response.text)
+print(dati)
+
